0ebc38de9b810c3498214cac76e2bc9cdc0d472a
Lily Dell <lily.starleks@gmail.com>
2023-01-26 20:17:00 +0000

    starter js file

---
+++ script.js
@@ -0,0 +1,25 @@
+$("search-button").on("click", function() {
+// get the id name of the search button
+  
+    let searchTerm = $(".searchTerm").val();
+    let numberRecords = $(".numberRecords").val();
+    let startYear = $(".startYear").val();
+    let endYear = $(".endYear").val();
+
+
+    let apiKey = 'mL4xkOC4g5YsgMhD0NXUQ33AGnerRi6W'
+    var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" +  +"&api-key=" + apiKey;
+
+    //"https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&fq=source:(%22The%20New%20York%20Times%22)&api-key=e6HGKf3D9XB4mlYWfTND3jMydbGGLxOq"
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response) {
+        console.log(response);
+
+
+
+
+    });
+});
