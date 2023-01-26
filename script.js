37ec1aa620c4832e75eee657403d21dbf3a4d8ab
Lily Dell <lily.starleks@gmail.com>
2023-01-26 20:42:13 +0000

    add ajax call and variables to store the api response

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
-$("search-button").on("click", function() {
+// $("button").on("click", function() {
 // get the id name of the search button
   
-    let searchTerm = $(".searchTerm").val();
-    let numberRecords = $(".numberRecords").val();
-    let startYear = $(".startYear").val();
-    let endYear = $(".endYear").val();
+    // let searchTerm = $(".searchTerm").val();
+    // let numberRecords = $(".numberRecords").val();
+    // let startYear = $(".startYear").val();
+    // let endYear = $(".endYear").val();
 
 
-    let apiKey = 'mL4xkOC4g5YsgMhD0NXUQ33AGnerRi6W'
+    // let apiKey = 'mL4xkOC4g5YsgMhD0NXUQ33AGnerRi6W'
     var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=finance&fq=source:(%22The%20New%20York%20Times%22)&begin_date=20220101&end_date=20230126&fq=coding&page=5&sort=newest&api-key=e6HGKf3D9XB4mlYWfTND3jMydbGGLxOq"
     
     //"https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + "?" +"&api-key=" + apiKey;
 
     //"https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&fq=source:(%22The%20New%20York%20Times%22)&api-key=e6HGKf3D9XB4mlYWfTND3jMydbGGLxOq"
+
 
     $.ajax({
         url: queryURL,
@@ -20,8 +21,25 @@ $("search-button").on("click", function() {
     }).then(function(response) {
         console.log(response);
 
+        let results = response.response.docs;
+        console.log(results);
+
+        results.forEach(function(element) {
+
+            let newsDiv = $('<div>');
+            let newsTitle = $(`<h1>${results.headline}</h1?`);
+            // let newsCategory = $(`<h2>${results.snippet}</h2>`);
+            let newsSnippet = $(`<p>${results.snippet}</h2>`);
+
+            newsDiv.append(newsTitle, newsSnippet);
+            $(".articles")
+            //
+        });
+
+
+
 
 
 
     });
-});
+// });
